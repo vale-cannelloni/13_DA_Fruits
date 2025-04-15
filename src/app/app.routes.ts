b5fb288c7e53edddc62746e4a212ea61ad4b6b3a
Valentino Strebel <tino_schweizer@hotmail.com>
2025-04-15 08:56:46 +0200

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { FruitlistComponent } from './fruitlist/fruitlist.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', component: FruitlistComponent }];
