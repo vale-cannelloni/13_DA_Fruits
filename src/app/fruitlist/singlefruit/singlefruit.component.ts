6d98cf766190a1bd0eeba3af513b91606c5a6d71
Valentino Strebel <tino_schweizer@hotmail.com>
2025-04-22 05:56:00 +0200

    update

---
+++ src/app/fruitlist/singlefruit/singlefruit.component.ts
@@ -0,0 +1,33 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-singlefruit',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './singlefruit.component.html',
+  styleUrl: './singlefruit.component.scss',
+})
+export class SinglefruitComponent {
+  @Input() fruit = {
+    name: 'Apfel',
+    img: 'apple.png',
+    description:
+      'Äpfel sind aufgrund ihres hohen Wassergehalts kalorienarm und enthalten nur Spuren von Fett und Eiweiß, dafür aber rund zwei Prozent Ballaststoffe und etwa elf Prozent Kohlenhydrate. Äpfel enthalten auch viele Vitamine und Mineralstoffe und sind daher eine wichtige Quelle für uns - zum Beispiel für Vitamin C.',
+    genus: 'Kernobstgewächsen innerhalb der Familie der Rosengewächse',
+    stars: 2.3,
+    reviews: [
+      { name: 'Kevin W.', text: 'ist lecker' },
+      { name: 'Arne P.', text: 'nicht so meins' },
+    ],
+  };
+
+  inputData = '';
+
+  @Output() fruitname = new EventEmitter<string>();
+
+  sendInputData() {
+    this.fruitname.emit(this.inputData);
+    this.inputData = '';
+  }
+}
